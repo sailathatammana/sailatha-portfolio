3ccb80b5c67d953a785dd6a7332f045f9d430f1c
sailathatammana <32009979+sailathatammana@users.noreply.github.com>
2021-09-03 13:51:42 +0200

    Desktop modal style updated

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -25,21 +25,27 @@ export default function ProjectCard({ information }) {
       </div>
       <Modal open={isOpen} onClose={() => setIsOpen(false)}>
         <div id="modal-popup">
-          <img src={screenshotURL} alt="" />
-          <h2>{information.title}</h2>
-          <p>{information.description}</p>
-          <Pills technologies={information.technologies} />
-          <div className="buttons">
-            <button className="purple-button">
-              <a target="blank" href={information.websiteURL}>
-                Visit website/app
-              </a>
-            </button>
-            <button className="white-button">
-              <a target="blank" href={information.githubURL}>
-                Git repository
-              </a>
-            </button>
+          <div className="modal-grid">
+            <div>
+              <img src={screenshotURL} alt="" />
+            </div>
+            <div>
+              <h2>{information.title}</h2>
+              <p>{information.description}</p>
+              <Pills technologies={information.technologies} />
+              <div className="buttons">
+                <button className="purple-button">
+                  <a target="blank" href={information.websiteURL}>
+                    Visit website/app
+                  </a>
+                </button>
+                <button className="white-button">
+                  <a target="blank" href={information.githubURL}>
+                    Git repository
+                  </a>
+                </button>
+              </div>
+            </div>
           </div>
         </div>
       </Modal>
