770f9516fccf3b2a1cb748793ea9bb31db5ac586
Eduardo <el_alienx@hotmail.com>
2021-09-14 13:18:00 +0200

    add space

--- src/sections/Technologies.jsx
+++ src/sections/Technologies.jsx
@@ -5,6 +5,7 @@ export default function Technologies() {
   const TechnologiesList = TechnologiesData.map((item) => (
     <TechnologiesCard key={item.id} title={item.title} logo={item.logo} />
   ));
+  
   return (
     <div className="content-section" id="tech">
       <h2>Technologies I use</h2>
